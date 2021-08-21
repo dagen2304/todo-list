962164ded779ba620e72cb0a44f7e268995509b9
hyacinthe goula <kemenegoula@gmail.com>
2021-08-22 00:09:45 +0100

    MAJ

--- config/DB.js
+++ config/DB.js
@@ -1,7 +1,7 @@
 const DB_PORT = process.env.DB_PORT
 const DB_HOST = process.env.DB_HOST
 const DB_NAME = process.env.DB_NAME
-const DB_PWD = process.env.DB_NAME
+const DB_PWD = process.env.DB_PWD
 
 module.exports = {
     conStr : "mongodb://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME,
